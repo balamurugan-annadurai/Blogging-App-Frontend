d13827860b6e4f5eee5142aa1693d7ca1e39ed5d
balamurugan-annadurai <abalamurugan2003@gmail.com>
2025-05-18 17:55:30 +0530

    feat: update API base URL

--- src/lib/services/api.config.js
+++ src/lib/services/api.config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/",
+  baseURL: "https://blogging-app-backend-lcc7.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
